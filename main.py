92281c9586d99845a87010d45bd0980626aa9107
Dipansu088 <dipunrikun2004@gmail.com>
2026-08-21 23:49:12 +0530

    Add JSON data persistence

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from datetime import date
+import json
 
 expenses=[]
+
+def load_expenses():
+    global expenses
+    try:
+        with open("expenses.json","r") as file:
+            expenses=json.load(file)
+    except FileNotFoundError:
+        expenses=[]
+        
+def save_expenses():
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
 def menu():
     print("==============| EXPENSE TRACKER |==============")
     print('''         
@@ -13,7 +27,7 @@ def menu():
          ''')
     
 def add_expense():
-    print("=========| ADD EXPENSE |=========")
+    print("=========| ADD EXPENSE |=========\n")
     while True:
         amt=input("Enter amount (in Rs): ")
         try:
@@ -68,11 +82,12 @@ def add_expense():
     "description":description,
     "date":expense_date
     })
-    print("Expense added successfully!")
+    save_expenses()
+    print("Expense added successfully!\n")
     
 def view_expenses():
     if not expenses:
-        print("No expenses available!!!")
+        print("No expenses available to display!!!")
     else:
         print("=========| YOUR EXPENSES |=========\n")
         for number, expense_item in enumerate(expenses, start=1):
@@ -101,6 +116,7 @@ def delete_expense():
                     if n>=1 and n<=len(expenses):
                         actual_index=n-1
                         deleted_expense=expenses.pop(actual_index)
+                        save_expenses()
                         print(f"Expense deleted successfully!\n")
                         print(f"Category: {deleted_expense['category']}")
                         print(f"Description: {deleted_expense['description']}")
@@ -114,16 +130,16 @@ def delete_expense():
 
 def view_total():
     if not expenses:
-        print("No expenses available to show total!!")
+        print("No expenses available to show total!!\n")
     else:
         total=0
         for i in expenses:
             total=total+i['amount']
-        print(f"Your total expense is: Rs {total:.2f}")
+        print(f"Your total expense: Rs {total:.2f}\n")
 
 def view_summary():
     if not expenses:
-        print("No expenses available for summary!")
+        print("No expenses available for summary!\n")
     else:
         summary={}
         for i in expenses:
@@ -133,32 +149,28 @@ def view_summary():
                 summary[category] = summary[category]+amount
             else:
                 summary[category]=amount
-        print("=========| EXPENSE SUMMARY |=========")
+        print("=========| EXPENSE SUMMARY |=========\n")
         for category,total in summary.items():
             print(f"{category}: Rs {total:.2f}")
    
+load_expenses()
 while True:    
     menu()
     try:
-        choice=int(input("Enter your choice: "))
+        choice=int(input(f"Enter your choice: "))
         if choice==1:
-            print("Add Expense Selected")
             add_expense()
             
         elif choice==2:
-            print("View Expense Selected")
             view_expenses()
             
         elif choice==3:
-            print("Delete Expense Selected")
             delete_expense()
             
         elif choice==4:
-            print("View Total Selected")
             view_total()
             
         elif choice==5:
-            print("View Summary Selected")
             view_summary()
             
         elif choice==6:
@@ -166,6 +178,6 @@ while True:
             break
         
         else:
-            print("Oops!!! Enter choice between 1 and 6!")
+            print("Oops!!! Enter choice between 1 and 6!\n")
     except ValueError:
-        print("Oops!!! Enter integer between 1 and 6!")
+        print("Oops!!! Enter integer between 1 and 6!\n")
